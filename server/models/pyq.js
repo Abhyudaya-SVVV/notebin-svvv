328db60f5e1dee81f28dc021f5bef748d7912e15
ayush-maddhesiya <ayushkumarmaddhesiya.com>
2024-10-18 00:11:32 +0530

    change in data model to improve

--- server/models/pyq.js
+++ server/models/pyq.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
+const { stringify } = require("querystring");
 
-const fileSchema = new mongoose.Schema(
+const pyqSchema = new mongoose.Schema(
   {
     filename: { 
       type: String, 
@@ -10,13 +11,17 @@ const fileSchema = new mongoose.Schema(
       type: String, 
       required: true 
     },
-    title: { 
+    unit : { 
       type: String, 
       required: true 
     },
     subject: { 
       type: String, 
       required: true 
+    },
+    subjectCode:{
+      type: String, 
+      required: false
     },
     semester: { 
       type: String, 
@@ -38,10 +43,17 @@ const fileSchema = new mongoose.Schema(
         required: true,
       },
     ],
+    exam:{
+      enum :[mst1,mst2.endsem]
+    },
+    year:{
+      type:string,
+      required:true
+    }
   },
   {
     timestamps: true,
   }
 );
 
-module.exports = mongoose.model("File", fileSchema);
+module.exports = mongoose.model("Pyq", pyqSchema)
---
+++ server/models/tags.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const tagSchema = new mongoose.Schema(
+  {name: {
+    type: String,
+    required: true,
+    unique: true
+  }
+},
+  {
+    timestamps: true
+  }
+)
+
+export default mongoose.model("Tag", tagSchema)
